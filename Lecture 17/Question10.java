903c8551ae5a4cbe9e048526bcdb11255bacaa6a
Tripurari Kumar <110098572+tripurari13@users.noreply.github.com>
2024-02-14 00:55:19 +0530

    Question10.java

--- Lecture 17/Question10.java
+++ Lecture 17/Question10.java
@@ -17,100 +17,35 @@ Note: Please don't change the package name.
 
 package q10891;
 public class Armstrong
+	import java.util.Scanner;
 
-{
-	
-	
-	
-	static boolean isArmstrong(int n)
-	
-	{
-		
-		int temp, digits=0, last=0, sum=0;
-		
-		
-		
-		temp=n;
-		
-		
-		
-		while(temp>0)
-		
-		{
-			
-			temp = temp/10;
-			
-			digits++;
-			
-		}
-		
-		temp = n;
-		
-		while(temp>0)
-		
-		{
-			
-			
-			
-			last = temp % 10;
-			
-			
-			
-			sum +=  (Math.pow(last, digits));
-			
-			//removes the last digit
-			
-			temp = temp/10;
-			
-		}
-		
-		
-		
-		if(n==sum)
-		
-		
-		
-		return true;
-		
-		
-		
-		else return false;
-		
-	}
-	
-	//driver code
-	
-	public static void  main(String args[])
-	
-	{
-		
-		int num=Integer.parseInt(args[0]);
-		
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        if(isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+    }
+    
+    public static boolean isArmstrong(int number) {
+        int originalNumber, remainder, result = 0, n = 0;
+        originalNumber = number;
+        while (originalNumber != 0) {
+            originalNumber /= 10;
+            ++n;
+        }
+        originalNumber = number;
+        while (originalNumber != 0) {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder, n);
+            originalNumber /= 10;
+        }
+        return result == number;
+    }
+}
 
-		
-	
-		
-		
-		
-	
-		
-		if(isArmstrong(num))
-		
-		{
-			
-			System.out.println("The given number " +num+ " is an armstrong number");
-			
-		}
-		
-		else
-		
-		{
-			
-			System.out.println("The given number " + num + " is not an armstrong number");
-			
-		}
-		
-	}
-	
-}  
 
